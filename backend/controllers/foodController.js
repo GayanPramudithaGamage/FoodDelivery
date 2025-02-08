f61bb1ed6aeb274fe62c76edb68a6aa92381462e
Gayan Gamage <gayan.2019561@iit.ac.lk>
2025-02-08 17:43:44 +0530

    Implement food management features: add, list, and remove food items

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,15 +1,54 @@
 import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
-//add food item 
+//add food item
 
 const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
 
-    let image_filename = '${req.file.filename}';
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
 
-    const food = new foodModel({
-         
-    })
+  try {
+    await food.save();
+    res.json({ succsess: true, message: "Food added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ succsess: false, message: "Failed to add food" });
+  }
 };
 
-export { addFood };
+//all food list
+
+const listfood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ succsess: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ succsess: false, message: "Failed to get food" });
+  }
+};
+
+// reomve food item
+
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {}); //delete image from uploads folder
+
+    await foodModel.findByIdAndDelete(req.body.id); //delete food item from database
+
+    res.json({ succsess: true, message: "Food removed successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ succsess: false, message: "Failed to remove food" });
+  }
+};
+
+export { addFood, listfood, removeFood };
