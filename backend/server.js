cc3b032b8b7ca786eea9c65a771442a2c151bb6a
Gayan Gamage <gayan.2019561@iit.ac.lk>
2025-02-10 22:54:31 +0530

    Set up initial admin panel structure with routing, styles and connect backend add, list item

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import cors from 'cors';
 import { connect } from 'mongoose';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 //app config
 const app = express();
@@ -18,6 +22,9 @@ connectDB();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images", express.static("uploads"));
+app.use("/api/user",userRouter)
+
+
 app.get("/", (req, res) => {
     res.send("API working");
 })
